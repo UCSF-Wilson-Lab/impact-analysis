c5def0a12baa7b57b7d2b1b959c2af219e5534a0
RaviDandekar <ravi.dandekar@gmail.com>
2024-05-29 15:46:24 -0700

    finished preliminary code to integrate GEX and CSP

--- code/1_create_combined_sc_object.R
+++ code/1_create_combined_sc_object.R
@@ -55,11 +55,12 @@ run_metadata_gex_list <- split(metadata_gex,metadata_gex$run_group)
 run_metadata_csp_list <- split(metadata_csp,metadata_csp$run_group)
 
 
-# 2. Create Seurat Objects for each batch
+# 2. Create Seurat Objects for each batch ----
 
 # makeRunInputMtx
 # - output.type = c("gex","csp")
-makeRunInputMtx <- function(runID ,run_metadata_list,THREADS,output.type = "gex") {
+makeRunInputMtx <- function(runID ,run_metadata_list,THREADS,
+                            output.type = "gex",min.genes.gex=700,min.genes.csp=9) {
   run          <- tstrsplit(runID,":")[[1]]
   type         <- tstrsplit(runID,":")[[2]]
   multi.status <- FALSE
@@ -81,8 +82,11 @@ makeRunInputMtx <- function(runID ,run_metadata_list,THREADS,output.type = "gex"
   if(THREADS > length(samples.vec)){
     THREADS <- length(samples.vec)
   }
+  min_genes <- 0
+  if(output.type == "gex"){min_genes <- min.genes.gex}
+  if(output.type == "csp"){min_genes <- min.genes.csp}
   gex.matrix <- generateCombinedMatrix(dataset_loc, samples.vec,THREADS = THREADS,multi.results = multi.status,
-                                       assay = output.type,min.genes.per.cell = 700,max.genes.per.cell = NULL)
+                                       assay = output.type,min.genes.per.cell = min_genes,max.genes.per.cell = NULL)
   
   return(gex.matrix)
 }
@@ -90,9 +94,60 @@ makeRunInputMtx <- function(runID ,run_metadata_list,THREADS,output.type = "gex"
 gex_run_list <- names(run_metadata_gex_list) %>% as.list()
 csp_run_list <- names(run_metadata_csp_list) %>% as.list()
 
+# CSP input matrix
 csp_mtx_list <- lapply(csp_run_list, makeRunInputMtx,
                        run_metadata_list=run_metadata_csp_list,
-                       THREADS=THREADS,output.type = "csp")
+                       THREADS=THREADS,output.type = "csp",min.genes.csp=1)
+merged_csp_mtx <- do.call("cbind",csp_mtx_list)
+
+# GEX input matrix
+gex_mtx_list <- lapply(gex_run_list, makeRunInputMtx,
+                       run_metadata_list=run_metadata_gex_list,
+                       THREADS=THREADS,output.type = "gex",min.genes.gex=400)
+merged_gex_mtx <- do.call("cbind",gex_mtx_list)
 
 
+
+# 3. Create Seurat Objects for GEX and CSP ----
+seurat.obj <- createSeuratObjectFromMatrix(
+  sc.data      = merged_gex_mtx,
+  project.name = "GEX_IMPACT_OnePatient",
+  npca         = 20, min.genes = 400,
+  normalize = F,dim.reduction = F
+)
+
+# Add samples column
+cells      <- row.names(seurat.obj@meta.data)
+sample_col <- cells
+sample_col <- tstrsplit(sample_col,"-")[[2]]
+seurat.obj@meta.data$sample <- sample_col
+
+# Omit samples with low number of cells
+LOW_CELLS_THRESH          <- 100
+sample_cell_counts        <- as.data.frame(table(seurat.obj$sample))
+names(sample_cell_counts) <- c("sample","Freq")
+sample_cell_counts        <- sample_cell_counts[sample_cell_counts$Freq > LOW_CELLS_THRESH,]
+
+cells_to_keep  <- names(seurat.obj$sample[seurat.obj$sample %in% sample_cell_counts$sample])
+seurat.obj     <- seurat.obj[,colnames(seurat.obj) %in% cells_to_keep]
+
+# 4. Identify Doublets ----
+set.seed(1234)
+
+seurat.obj <- findDoublets(seurat.obj,sample.col = "sample",threads = THREADS)
+
+# 5. Add in CSP and overlap both assays ----
+cells_csp <- colnames(merged_csp_mtx)
+cells_gex <- colnames(seurat.obj)
+overlap   <- cells_csp[cells_csp %in% cells_gex]
+
+# Only keep overlapping cells
+seurat.obj <- seurat.obj[,colnames(seurat.obj) %in% overlap]
+counts_csp <- merged_csp_mtx[,colnames(merged_csp_mtx) %in% overlap]
+
+seurat.obj[["CSP"]] <- CreateAssayObject(counts = counts_csp)
+
+
+# SAVE ----
+save(seurat.obj,file = fh_raw_seurat_obj)
 
